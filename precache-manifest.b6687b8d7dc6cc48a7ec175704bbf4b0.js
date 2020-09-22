5baa8ead409e5ed4b17211f6b4ddeef22eede3ee
Karan Shah <shahk0927@gmail.com>
2020-09-21 19:30:32 -0700

    Updates

---
+++ precache-manifest.b6687b8d7dc6cc48a7ec175704bbf4b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aef83c1e69fa87b8fd65cb4d4128121",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "4d3f7f24f82527636ede",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "720a5365afa4cdff1363",
+    "url": "/react-deploy/static/js/2.f0e9eb04.chunk.js"
+  },
+  {
+    "revision": "b95d735f9bdac369c512281af49352ca",
+    "url": "/react-deploy/static/js/2.f0e9eb04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d3f7f24f82527636ede",
+    "url": "/react-deploy/static/js/main.fb15fe72.chunk.js"
+  },
+  {
+    "revision": "f1288e21cf3458eeea7e",
+    "url": "/react-deploy/static/js/runtime-main.5cf7a7cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
